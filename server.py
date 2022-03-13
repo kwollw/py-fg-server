08c9bf5461ed764e7adb65658c6af4535484b693
Kai Wollweber <wollw@ki.tng.de>
2022-03-13 22:50:44 +0100

    remove print

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-import bottle
+from bottle import app, request, response
 from bottle.ext import sqlite
 from bottle_cors_plugin import cors_plugin
 import json
 
-app = bottle.app()
+app = app()
 app.install(sqlite.Plugin(dbfile='fahrplan.sqlite3'))
 app.install(cors_plugin('*'))
 
@@ -11,19 +11,15 @@ app.install(cors_plugin('*'))
 def users(db):  
   cur = db.execute('select * from members where active = "true" order by sirname')
   result = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
-  bottle.response.content_type = 'application/json'
+  response.content_type = 'application/json'
   return json.dumps({'message': 'success', 'users': result})
 
-@app.route('/api2/authenticate')
-def hello():
-    return "Hello World!"
-
-@app.route('/api2/schedule')
-def hello():
-    return "Hello World!"   
-
-@app.route('/api2/reset')
-def hello():
-    return "Hello World!"
+@app.route('/api2/schedule', method='POST')
+def schedule(db):  
+  data = request.json
+  cur = db.execute("select * from schedule where Date = :date", data)
+  result = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+  response.content_type = 'application/json'
+  return json.dumps({'message': 'success', 'data': result})
 
 app.run(host='localhost', port=4001, debug=True)
