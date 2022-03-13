d0d0f41adc95dcd2d84ab06d893cb0a827978fd8
Kai Wollweber <wollw@ki.tng.de>
2022-03-13 18:33:09 +0100

    add cors

--- server.py
+++ server.py
@@ -1,7 +1,29 @@
-from bottle import route, run
+import bottle
+from bottle.ext import sqlite
+from bottle_cors_plugin import cors_plugin
+import json
 
-@route('/api2/hello')
+app = bottle.app()
+app.install(sqlite.Plugin(dbfile='fahrplan.sqlite3'))
+app.install(cors_plugin('*'))
+
+@app.route('/api2/users', method='GET')
+def users(db):  
+  cur = db.execute('select * from members where active = "true" order by sirname')
+  result = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+  bottle.response.content_type = 'application/json'
+  return json.dumps({'message': 'success', 'users': result})
+
+@app.route('/api2/authenticate')
 def hello():
     return "Hello World!"
 
-run(host='localhost', port=4001, debug=True)
+@app.route('/api2/schedule')
+def hello():
+    return "Hello World!"   
+
+@app.route('/api2/reset')
+def hello():
+    return "Hello World!"
+
+app.run(host='localhost', port=4001, debug=True)
