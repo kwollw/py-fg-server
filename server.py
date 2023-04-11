58f9e9257191c24ff7f6d034ffe7deac68f87fd6
Kai Wollweber <wollw@ki.tng.de>
2023-04-11 15:42:07 +0200

    login with jwt

--- server.py
+++ server.py
@@ -1,48 +1,55 @@
 from bottle import *
-from JwtAuth import JwtAuth
+import jwt
 from config import issuer, audience, jwt_config, salt
 
-from bottle.ext import sqlite
+import sqlite3
+
 from bottle_cors_plugin import cors_plugin
 import json
 import hashlib
 
-app = Bottle()
-app.install(sqlite.Plugin(dbfile='fahrplan.sqlite3'))
+db = sqlite3.connect('fahrplan.sqlite3')
+db.row_factory = sqlite3.Row
 
-app.install(cors_plugin('*'))
+install(cors_plugin('*'))
 
 
 # Create a restriction on any route with a prefix of '/api/'
-# aud=JwtAuth(jwt_config = jwt_config, issuer=issuer, audience=audience, url_prefix='/')
+# aud=JwtAuth(jwt_config = jwt_config, issuer=issuer, audience=audience, url_prefix='/api/')
 
 # app.install(aud)
 
-@app.route('/api/users/authenticate', method='POST')
-def authenticate(db):  
+@route('/api/users/authenticate', method='POST')
+def authenticate():  
   user = request.json['username']
   password = hashlib.sha1((request.json['password'] + salt).encode()).hexdigest()
   print(user, password)
   cur = db.execute('select * from members where (user like ? or mail like ?) and password_sha1 = ?',[user, user, password])
   valid_user = cur.fetchall()
-  print(valid_user)
+  if len(valid_user) == 1:
+    print('successful login for ', valid_user[0]['user'], valid_user[0]['name'], valid_user[0]['groupID'])
+    token = jwt.encode({"some": "payload"}, jwt_config["key"], algorithm="HS256")
+    response.headers['Content-Type'] = 'application/json'
+    return json.dumps({ "message":"success","token":token, "data":valid_user[0]['name']})
+  else:
+    print('login failed')
+    response.headers['Content-Type'] = 'application/json'
+    return json.dumps({ "message":"failed"})
 
 
-
-
-@app.route('/api/users', method='GET')
-def users(db):  
+@route('/api/users', method='GET')
+def users():  
   cur = db.execute('select * from members where active = "true" order by sirname')
   result = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
   response.content_type = 'application/json'
   return json.dumps({'message': 'success', 'users': result})
 
-@app.route('/api/schedule', method='POST')
-def schedule(db):  
+@route('/api/schedule', method='POST')
+def schedule():  
   data = request.json
   cur = db.execute("select * from schedule where Date = :date", data)
   result = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
   response.content_type = 'application/json'
   return json.dumps({'message': 'success', 'data': result})
 
-app.run(host='localhost', port=4000, debug=True)
+run(host='localhost', port=4000, debug=True)
