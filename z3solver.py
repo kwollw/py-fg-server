9420280040ea30febc0b63979f2019ad42b241fb
Kai Wollweber <wollw@ki.tng.de>
2023-04-16 00:35:29 +0200

    z3 select slots

--- z3solver.py
+++ z3solver.py
@@ -13,67 +13,78 @@ def minute_diff(fro,to):
   t = to.split(":")
   return int(t[0])*60 + int(t[1]) - (int(f[0])*60 + int(f[1]))
 
-requests = db.getactive_requests("2023-03-07")
-slots_to = get_slots(requests,"time_to")
-slots_fro = get_slots(requests,"time_fro")
+def schedule(date):
+  requests = db.getactive_requests(date)
+  #Pseudofahrer zur Aufnahme nicht erfüllbarer Mitfahrer:
+  requests.append({'user': 'NULL', 'driver_status': 'F', 'time_to': '0:00', 'time_to_max_delay': 0, 'max_passengers_to': 1000, 'time_fro': '24:00', 'time_fro_max_delay': 0, 'max_passengers_fro': 1000, 'passengers_count': 0, 'rides_count': 0})
+  slots_to = get_slots(requests,"time_to")
+  slots_fro = get_slots(requests,"time_fro")
 
-drive = [ Int("drive_%s" % (i)) for i in range(len(requests))]
-delay_to = [ Int("delay_to_%s" % (i)) for i in range(len(requests))]
-delay_fro = [ Int("delay_fro_%s" % (i)) for i in range(len(requests))]
-
-
-a = []
-for index, req in enumerate(requests):
-  cost = max(5*(req['passengers_count']-req['rides_count'])+1000, 500)
-  if req["driver_status"]=="F":
-    a.append(drive[index] == cost)
-  elif req["driver_status"]=="M":
-    a.append(drive[index] == 0)
-  else:
-    a.append(Or(drive[index]== 0, drive[index] == cost))
-  if req['time_to'] != "":
-    delays = []
-    for slot in slots_to:
-      delay = minute_diff(req['time_to'],slot['time_to'])
-      if (req['driver_status'] == "M" or delay <= req['time_to_max_delay']) and delay >= 0:
-        delays.append(delay_to[index] == delay)
-    a.append(Or (delays))
-  if req['time_fro'] != "":
-    delays = []
-    for slot in slots_fro:
-      delay = minute_diff(slot['time_fro'], req['time_fro'])
-      if (req['driver_status'] == "M" or delay <= req['time_to_max_delay']) and delay >= 0:
-        delays.append(delay_fro[index] == delay)
-    a.append(Or (delays))
+  drive = [ Int("drive_%s" % (i)) for i in range(len(requests))]
+  delay_to = [ Int("delay_to_%s" % (i)) for i in range(len(requests))]
+  delay_fro = [ Int("delay_fro_%s" % (i)) for i in range(len(requests))]
 
 
-for slot in slots_to:
-  seats = []
+  a = []
   for index, req in enumerate(requests):
+    cost = max(5*(req['passengers_count']-req['rides_count'])+1000, 500)
+    if req["driver_status"]=="F":
+      a.append(drive[index] == cost)
+    elif req["driver_status"]=="M":
+      a.append(drive[index] == 0)
+    else:
+      a.append(Or(drive[index]== 0, drive[index] == cost))
     if req['time_to'] != "":
-      delay = minute_diff(req['time_to'],slot['time_to'])
-      if (req['driver_status'] == "M" or delay <= req['time_to_max_delay']) and delay >= 0:
-        seats.append(If(delay_to[index] == delay, If(drive[index] == 0, -1, req['max_passengers_to']), 0))
-  a.append(Sum(seats) <= 0)
-
-for slot in slots_fro:
-  seats = []
-  for index, req in enumerate(requests):
+      delays = []
+      for slot in slots_to:
+        delay = minute_diff(slot['time_to'], req['time_to'])
+        if index == 16:
+          print(req['time_to'], slot['time_to'], delay)
+        if (req['driver_status'] == "M" or delay <= req['time_to_max_delay']) and delay >= 0:
+          delays.append(delay_to[index] == delay)
+      a.append(Or (delays))
     if req['time_fro'] != "":
-      delay = minute_diff(slot['time_fro'],req['time_fro'])
-      if (req['driver_status'] == "M" or delay <= req['time_fro_max_delay']) and delay >= 0:
-        seats.append(If(delay_fro[index] == delay, If(drive[index] == 0, -1, req['max_passengers_fro']), 0))
-  a.append(Sum(seats) <= 0)
+      delays = []
+      for slot in slots_fro:
+        delay = minute_diff(req['time_fro'], slot['time_fro'])
+        if (req['driver_status'] == "M" or delay <= req['time_to_max_delay']) and delay >= 0:
+          delays.append(delay_fro[index] == delay)
+      a.append(Or(delays))
 
-cost = Int('cost') 
 
-opt = Optimize()
-opt.add(a)
-opt.add(cost == sum(drive) + sum(delay_to) + sum(delay_fro))
-opt.minimize(cost)
-print(opt.check())
-solution = opt.model()
-print((cost,solution[cost]))
-print([(var,solution[var]) for var in drive])
-print([(var,solution[var]) for var in delay_to])
-print([(var,solution[var]) for var in delay_fro])
+  for slot in slots_to:
+    seats = []
+    for index, req in enumerate(requests):
+      if req['time_to'] != "":
+        delay = minute_diff(slot['time_to'],req['time_to'])
+        if (req['driver_status'] == "M" or delay <= req['time_to_max_delay']) and delay >= 0:
+          seats.append(If(delay_to[index] == delay, If(drive[index] == 0, -1, req['max_passengers_to']), 0))
+    a.append(Sum(seats) >= 0)
+
+  for slot in slots_fro:
+    seats = []
+    for index, req in enumerate(requests):
+      if req['time_fro'] != "":
+        delay = minute_diff(req['time_fro'],slot['time_fro'])
+        if (req['driver_status'] == "M" or delay <= req['time_fro_max_delay']) and delay >= 0:
+          seats.append(If(delay_fro[index] == delay, If(drive[index] == 0, -1, req['max_passengers_fro']), 0))
+    a.append(Sum(seats) >= 0)
+
+  cost = Int('cost')
+
+  opt = Optimize()
+  opt.add(a)
+  opt.add(cost == Sum(drive) + Sum(delay_to) + Sum(delay_fro))
+  opt.minimize(cost)
+  check = opt.check()
+  for req in requests:
+    print(req['user'], req['driver_status'], req['time_to'], req['time_to_max_delay'], req['max_passengers_to'], req['time_fro'], req['time_fro_max_delay'], req['max_passengers_fro'])
+  # print(a)
+  if check == sat: 
+    solution = opt.model()
+    print((cost,solution[cost]))
+    print([(var,solution[var]) for var in drive])
+    print([(var,solution[var]) for var in delay_to])
+    print([(var,solution[var]) for var in delay_fro])
+  else:
+    print("unsat")
