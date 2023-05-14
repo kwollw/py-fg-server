fde0f45effe61772f6e6e151ebdbec3d3486955e
Kai Wollweber <wollw@ki.tng.de>
2023-05-14 22:16:33 +0200

    fixed into schedule

--- alter_db.sql
+++ alter_db.sql
@@ -35,22 +35,22 @@ CREATE VIEW rides_count AS SELECT groupID, Rider user, count() rides_count FROM 
 
 DROP VIEW schedule;
 CREATE VIEW schedule as 
-select d.groupID, d.Date, time_to time, 'to' direction, d.Driver, d.max_passengers_to max_passengers, count(*) passengers, group_concat(Rider, ', ') rider
+select d.groupID, d.Date, time_to time, 'to' direction, d.Driver, d.max_passengers_to max_passengers, count(*) passengers, group_concat(Rider, ', ') rider, d.fixed
 from drives d left join rides r on (d.groupID = r.groupID and d.Driver = r.Driver and d.Date = r.Date and time_to = time)
 where time is not NULL
 group by d.groupID, d.Date, time, direction, d.Driver, max_passengers
 UNION ALL
-select d.groupID, d.Date, time_fro time, 'fro' direction, d.Driver, d.max_passengers_fro max_passengers, count(*) passengers, group_concat(Rider, ', ') rider
+select d.groupID, d.Date, time_fro time, 'fro' direction, d.Driver, d.max_passengers_fro max_passengers, count(*) passengers, group_concat(Rider, ', ') rider, d.fixed
 from drives d left join rides r on (d.groupID = r.groupID and d.Driver = r.Driver and d.Date = r.Date and time_fro = time)
 where time is not NULL 
 group by d.groupID, d.Date, time, direction, d.Driver, max_passengers
 UNION
-select d.groupID, d.Date, time_to time, 'to' direction, d.Driver, d.max_passengers_to max_passengers, 0 passengers, rider
+select d.groupID, d.Date, time_to time, 'to' direction, d.Driver, d.max_passengers_to max_passengers, 0 passengers, rider, d.fixed
 from drives d left join rides r on (d.groupID = r.groupID and d.Driver = r.Driver and d.Date = r.Date and time_to = time)
 where rider is NULL and time_to <> ''
 group by d.groupID, d.Date, time, direction, d.Driver, max_passengers
 UNION
-select d.groupID, d.Date, time_fro time, 'fro' direction, d.Driver, d.max_passengers_fro max_passengers, 0 passengers, rider
+select d.groupID, d.Date, time_fro time, 'fro' direction, d.Driver, d.max_passengers_fro max_passengers, 0 passengers, rider, d.fixed
 from drives d left join rides r on (d.groupID = r.groupID and d.Driver = r.Driver and d.Date = r.Date and time_fro = time)
 where rider is NULL and time_fro <> ''
 group by d.groupID, d.Date, time, direction, d.Driver, max_passengers;
