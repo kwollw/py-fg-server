82b2c5fc840e1c08984af373d6ca732e8e6ad330
Kai Wollweber <wollw@ki.tng.de>
2023-10-19 23:14:47 +0200

    alter table members

--- alter_db.sql
+++ alter_db.sql
@@ -1,3 +1,12 @@
+DROP VIEW drives_count;
+DROP VIEW exceptions_view;
+DROP VIEW requests_view;
+DROP VIEW rides_count;
+DROP VIEW schedule;
+DROP VIEW total_drives;
+
+
+
 CREATE TABLE IF NOT EXISTS "groups" (
 	"groupID"	TEXT (5),
 	"description"	TEXT (30)
@@ -7,6 +16,28 @@ CREATE TABLE IF NOT EXISTS "schedule_head" (
 	"groupID"	TEXT(5),
 	"date"    TEXT(10)
 );
+
+CREATE TABLE members2(
+	user TEXT (5), 
+	groupID TEXT (5),
+	password_sha1 TEXT (30) NOT NULL, 
+	name TEXT (30), 
+	sirname TEXT (30), 
+	mobile TEXT (20), 
+	mail TEXT (30),
+	drives_count INTEGER, 
+	passengers_count INTEGER, 
+	rides_count INTEGER, 
+	active BOOLEAN DEFAULT true, 
+	role TEXT DEFAULT "user",
+	PRIMARY KEY(user, groupID)
+	);
+INSERT INTO members2
+  SELECT user, groupID, password_sha1, name, sirname, mobile, 
+	mail, drives_count, passengers_count, rides_count, active, role
+	FROM members;
+DROP TABLE members;
+ALTER TABLE members2 RENAME TO members;
 
 
 ALTER TABLE drives
@@ -22,23 +53,18 @@ ALTER TABLE rides
 	ADD "groupID"	TEXT(5);
 
 
-DROP VIEW drives_count;
 CREATE VIEW drives_count AS SELECT groupID, Driver user, count() drives_count FROM rides where fixed = true GROUP BY groupID, Driver;
 
-DROP VIEW exceptions_view;
 CREATE VIEW exceptions_view AS select a.*, rides_count, passengers_count 
 from exceptions a, members b
-where a.user = b.user;
+where a.user = b.user AND a.groupID = b.groupID;
 
-DROP VIEW requests_view;
 CREATE VIEW requests_view AS select a.*, rides_count, passengers_count 
 from requests a, members b
-where a.user = b.user;
+where a.user = b.user AND a.groupID = b.groupID;
 
-DROP VIEW rides_count;
 CREATE VIEW rides_count AS SELECT groupID, Rider user, count() rides_count FROM rides where fixed = true GROUP BY Rider;
 
-DROP VIEW schedule;
 CREATE VIEW schedule as 
 select d.groupID, d.Date, time_to time, 'to' direction, d.Driver, d.max_passengers_to max_passengers, count(*) passengers, group_concat(Rider, ', ') rider, d.fixed
 from drives d left join rides r on (d.groupID = r.groupID and d.Driver = r.Driver and d.Date = r.Date and time_to = time)
@@ -60,7 +86,6 @@ from drives d left join rides r on (d.groupID = r.groupID and d.Driver = r.Drive
 where rider is NULL and time_fro <> ''
 group by d.groupID, d.Date, time, direction, d.Driver, max_passengers;
 
-DROP VIEW total_drives;
 CREATE VIEW total_drives AS SELECT groupID, Driver user, count(*) drives FROM drives where fixed = true GROUP BY groupID, Driver;
 
 CREATE VIEW drive_dates as
